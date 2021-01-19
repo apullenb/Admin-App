b2c9260dd6b335e0a99587bb6a9015ecf3f18388
Lance Wurzbacher <Lance@zilis.com>
2021-01-19 01:06:50 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -158,6 +158,3 @@ render(){
   }
 };
 export default App;
-const requestListener = function (req, res) {res.writeHead(200); res.end('Hello, World!'); } 
-const server = http.createServer(requestListener); server.listen(8080)
-
