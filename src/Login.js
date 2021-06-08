ebea1bb2fc05a1250debd01194c535af2aae9be2
Andrea Pullen <Andrea.Pullen@zilis.com>
2021-06-08 13:58:21 -0500

    update login

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import styled from "styled-components";
-import PageWrapper from '../src/GlobalComponents/PageWrapper'
 
-function Login(props) {
+function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
@@ -15,58 +11,50 @@ function Login(props) {
   const handleChangeP = (e) => setPassword(e.target.value);
 
   const handleLogin = () => {
-      // Login function using Admin Authentication
-  }
+    // Login function using Admin Authentication
+  };
   return (
-     
     <FormWrapper>
-      <h2 className="common-page-title">ADMIN LOGIN</h2>
-      <Form>
-        <Form.Group controlId="formUserName">
-          <Form.Label>Admin User Name</Form.Label>
-          <Form.Control
-            type="username"
-            placeholder="Enter Username"
-            value={userName}
-            onChange={(e) => handleChangeU(e)}
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => handleChangeP(e)}
-          />
-        </Form.Group>
-        <Button
-          variant="primary"
-          onClick={handleLogin}
-        >
-          Login
-        </Button>
-      </Form>
-      <br />
+      <Title>ADMIN LOGIN</Title>
+      <Form.Control
+        type="username"
+        placeholder="Enter Username"
+        value={userName}
+        onChange={(e) => handleChangeU(e)}
+      />
+      <Form.Control
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => handleChangeP(e)}
+      />
+      <Button className="primary" onClick={handleLogin}>
+        Login
+      </Button>
     </FormWrapper>
-   
   );
 }
 
 export default Login;
 
-
 const FormWrapper = styled.div`
   display: flex;
   margin: 3%;
-  justify-content: center;
-  align-self: center;
-  align-items: center;
   flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
   border: 1px solid rgba(0, 0, 0, 0.5);
-  padding: 3%;
-  width: 40%;
+  padding: 2% 6%;
+  width: 35%;
+  height: 350px;
   border-radius: 5px;
   box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.3);
+`;
+
+const Title = styled.span`
+  text-align: center;
+  font-size: 40px;
+  color: rgb(92, 90, 90, 0.8);
+  font-weight: 450;
+  margin: 10px;
 `;
