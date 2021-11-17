1172ccc1823d996ade33b8d97f596f335fedd7dc
Andrea Pullen <70964079+apullenb@users.noreply.github.com>
2021-11-17 12:02:20 -0600

    pagination & sort

--- src/GlowChallenge/GlowEntry.js
+++ src/GlowChallenge/GlowEntry.js
@@ -1,11 +1,182 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import config from '../config/env-urls'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import axios from 'axios';
+import Moment from 'react-moment';
+import Select from 'react-select';
+import { useToasts } from "react-toast-notifications";
+import { LoginSkincareAdmin } from "../redux/actions/Skincare/skincareActions";
 
-function GlowEntry() {
+function GlowEntry(props) {
+    const entry = props.location.state
+    const [goals, setGoals] = useState([])
+    const [change, setChange] = useState(false);
+    const { skincareAuthToken } = useSelector(state => state.app);
+    const dispatch = useDispatch();
+    const { addToast } = useToasts();
+  
+ console.log(entry)
+
+
     return (
-        <div>
-            <h1>Glow Entry</h1>
-        </div>
-    )
-}
+      <div style={{margin:'0 8%'}}>
+        <h1>
+          Glow Challenge Entry 
+          </h1>
+         
+      
+        <EntryDetails>
+            <div className="page-header-link"><Link to="/Challenge/Glow-Entries">Back to list</Link></div>
+          <Row>
+            <Col>
+              <div>
+                <label>Entry ID</label>
+                <span className="read-only-value">{entry.glowEntryId}</span>
+              </div>
+              <div>
+                <label>Date Created</label>
+                <span className="read-only-value"><Moment format="MM/DD/YYYY">{entry.createdDate}</Moment></span>
+              </div>
+              <div>
+                <label>Challenge</label>
+                <span className="read-only-value">{entry.contestTitle}</span>
+              </div>
+              <div>
+                <label>Goals</label>
+                <div className="read-only-value" style={{marginTop:'5px'}}>
+                      <div >
+                        <input type="checkbox" checked={entry.goalWeight} />
+                              &nbsp; <span>Weight Management Support</span>
+                      </div>
+                      <div>
+                        <input type="checkbox" checked={entry.goalImmune} />
+                              &nbsp; <span>Promote a Healthy Immune System</span>
+                      </div>
+                      <div>
+                        <input type="checkbox" checked={entry.goalStress} />
+                              &nbsp; <span>Help with Situational Stress</span>
+                      </div>
+                      <div>
+                        <input type="checkbox" checked={entry.goalSleep} />
+                              &nbsp; <span>Aid Sleep During Restless Night</span>
+                      </div>
+                      <div>
+                        <input type="checkbox" checked={entry.goalOverall} />
+                              &nbsp; <span>Overall Well-Being</span>
+                      </div>
+                  
+                 
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div>
+                <label>Ambassador ID</label>
+                <span className="read-only-value">{entry.ambassadorId}</span>
+              </div>
+              <div>
+                <label>Name</label>
+                <span className="read-only-value">
+                      <Link to = {{ pathname: `/Challenge/Glow-Entries` }}>{entry.name}</Link></span>
+              </div>
+              <div>
+                <label>Email</label>
+                <span className="read-only-value">{entry.email}</span>
+              </div>
+              <div style={{border: '1px solid #707070', padding:'1px', marginTop:'5px'}}>
+               <GrayBox>
+                   <div>Scientific data is private, you can only see if information was submitted, not what was submitted.</div>
+                   <div >
+                        <input type="checkbox" checked={entry.height !== ''} />
+                              &nbsp; <span>Height</span>
+                      </div>
+                      <div >
+                        <input type="checkbox" checked={entry.gender !== ''} />
+                              &nbsp; <span>Gender</span>
+                      </div>
+               </GrayBox>
+
+               
+              </div>
+              
+            </Col>
+          </Row>
+          <hr />
+          <table>
+            <thead>
+            <tr>
+              <th className='head'>Submission ID</th>
+            </tr>
+            </thead>
+        </table>
+          <Row>
+           <Col></Col>
+           <Col></Col>
+           <Col></Col>
+           <Col></Col>
+           <Col><button>Delete</button>
+              <Success></Success>
+           </Col>
+           
+          </Row>
+        </EntryDetails>
+      </div>
+    );
+  }
+  
+ 
+  const GrayBox = styled.div`
+    border: 1px solid #707070;
+    padding: 15px;
+    font: normal normal normal 16px/27px Segoe UI;
+    color: #707070;
+  `
+  
+  
+  const ContestImage = styled.div`
+    display: inline-block;
+    max-width: 350px;
+    margin: 2px;
+    border: 1px solid #d8d8d8;
+    border-radius: 2px;
+    padding: 1%;
+    vertical-align: top;
+    img {
+      width: 100%;
+    }
+  `;
+  
+  const Success = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  margin: 6px 2%;
+  `
+  
+  const EntryDetails = styled.section`
+    color: rgb(94, 93, 93);
+    font-weight: 400;
+    font-size: 17px;
+    padding: 1px 1%;
+    text-align: left;
+  
+    label {
+      display: inline-block;
+      min-width: 150px;
+      vertical-align: top;
+    }
+  
+    .read-only-value {
+      display: inline-block;
+      margin: 0 10px;
+    }
+    
+  `;
 
 export default GlowEntry
--- src/GlowChallenge/GlowEntryList.js
+++ src/GlowChallenge/GlowEntryList.js
@@ -2,199 +2,285 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Pagination from "../SkincareChallenge/SCAccountList/Pagination";
-import '../SkincareChallenge/SCEntryList/EntryList.scss'
-import { CaretUp, CaretDown} from "react-bootstrap-icons";
-import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-import { getGlowEntries, filterAccounts} from './../redux/actions/Skincare/skincareActions';
-import { getEntries } from './../redux/actions/Skincare/skincareActions';
+import "../SkincareChallenge/SCEntryList/EntryList.scss";
+import { CaretUp, CaretDown } from "react-bootstrap-icons";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getGlowEntries,
+  filterAccounts,
+} from "./../redux/actions/Skincare/skincareActions";
+import { getEntries } from "./../redux/actions/Skincare/skincareActions";
 import GlowEntries from "./GlowEntry";
 
 function GlowEntryList() {
-    const [message, setMessage] = useState(true);
-    const [blank, setBlank] = useState(false);
-    const [pageNo, setPageNo] = useState(1);
-    const [perPage, setPerPage] = useState(10);
-    const [colSort, setColSort] = useState("id");
-    const [sortDirection, setSortDirection] = useState("asc");
-    const [localAccounts, setLocalAccounts ] = useState([]);
-    const [idInput, setIdInput] = useState(false);
-    const [nameInput, setNameInput] = useState(false);
-    const [emailInput, setEmailInput] = useState(false);
-    const [ambassadorIdInput, setAmbassadorIdInput] = useState(false);
-    const [challengeInput, setChallengeInput] = useState(false);
-    const [col, setCol] = useState("id");
-    const [filter, setFilter] = useState("");
-  
-    const dispatch = useDispatch();
-    const { entries } = useSelector(state => state.entries);
-  console.log(entries)
-    useEffect(() => {
-      dispatch(getGlowEntries());
-    }, []);
-  
-  
-    useEffect(() => {
-  setLocalAccounts(entries);
-    }, [entries]);
-  
-    
-    const accountsSort = (numPerPage, pageNoVal, sortInfo, sortBy) => {
-      setColSort(sortInfo);
-      setSortDirection(sortBy);
-      dispatch(filterAccounts(col, filter, numPerPage, pageNoVal, sortInfo, sortBy));
-    }
-  
-    const updatePerPage = (val) =>{
-      setPerPage(val);
-      dispatch(filterAccounts(col, filter, val, pageNo, colSort, sortDirection));
-    }
-  
-    const updatePageNo = (val) => {
-      setPageNo(val);
-      dispatch(filterAccounts(col, filter, perPage, val, colSort, sortDirection));
-    }
-  
-  
-  const handleChange = (e) => {
-  const filter = e.target.value;
-  const col = e.target.id;
-  setCol(e.target.id);
-  setFilter(e.target.value);
-  if (e.target.value === "") {
-    dispatch(getGlowEntries());
-  } else {
-    dispatch(filterAccounts(col, filter, perPage, pageNo, colSort, sortDirection));
-  }
+  const [message, setMessage] = useState(true);
+  const [blank, setBlank] = useState(false);
+  const [pageNo, setPageNo] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const [colSort, setColSort] = useState("glowEntryId");
+  const [sortDirection, setSortDirection] = useState("asc");
+  const [localAccounts, setLocalAccounts] = useState([]);
+  const [idInput, setIdInput] = useState(false);
+  const [nameInput, setNameInput] = useState(false);
+  const [emailInput, setEmailInput] = useState(false);
+  const [ambassadorIdInput, setAmbassadorIdInput] = useState(false);
+  const [challengeInput, setChallengeInput] = useState(false);
+  const [col, setCol] = useState("glowEntryId");
+  const [filter, setFilter] = useState("");
+
+  const dispatch = useDispatch();
+  const { entries } = useSelector((state) => state.entries);
+
+  useEffect(() => {
+    dispatch(getGlowEntries(perPage, pageNo, colSort, sortDirection));
+  }, []);
+
+  useEffect(() => {
+    setLocalAccounts(entries);
+  }, [entries]);
+
+  const accountsSort = (numPerPage, pageNoVal, sortInfo, sortBy) => {
+    setColSort(sortInfo);
+    setSortDirection(sortBy);
+    dispatch(getGlowEntries(numPerPage, pageNoVal, sortInfo, sortBy));
   };
-  
+
+  const updatePerPage = (val) => {
+    setPerPage(val);
+    dispatch(getGlowEntries(perPage, pageNo, colSort, sortDirection));
+  };
+
+  const updatePageNo = (val) => {
+    setPageNo(val);
+    dispatch(getGlowEntries(perPage, pageNo, colSort, sortDirection));
+  };
+
+  const handleChange = (e) => {
+      const column = e.target.id === col ? "glowEntryId" : e.target.id
+      setCol(column)
+      setColSort(e.target.id)
+    dispatch(getGlowEntries( perPage, pageNo, column, sortDirection));
+  };
+
   const disableInput = (e) => {
-   if (e.target.value === ""){
-    setNameInput(false);
-    setEmailInput(false);
-    setIdInput(false);
-    setAmbassadorIdInput(false);
+    if (e.target.value === "") {
+      setNameInput(false);
+      setEmailInput(false);
+      setIdInput(false);
+      setAmbassadorIdInput(false);
+    } else if (e.target.id === "glowEntryId") {
+      setNameInput(true);
+      setEmailInput(true);
+      setAmbassadorIdInput(true);
+    } else if (e.target.id === "name") {
+      setIdInput(true);
+      setEmailInput(true);
+      setAmbassadorIdInput(true);
+    } else if (e.target.id === "email") {
+      setNameInput(true);
+      setIdInput(true);
+      setAmbassadorIdInput(true);
+    } else if (e.target.id === "ambassadorID") {
+      setNameInput(true);
+      setEmailInput(true);
+      setIdInput(true);
     }
-  else if (e.target.id === "id") {
-    setNameInput(true);
-    setEmailInput(true);
-    setAmbassadorIdInput(true);
-  }
-  else if (e.target.id === "name") {
-    setIdInput(true);
-    setEmailInput(true);
-    setAmbassadorIdInput(true);
-  }
-  else if (e.target.id === "email") {
-    setNameInput(true);
-    setIdInput(true);
-    setAmbassadorIdInput(true);
-  }
-  else if (e.target.id === "ambassadorID") {
-    setNameInput(true);
-    setEmailInput(true);
-    setIdInput(true);
-  } 
-  
-  }
-  
-    return (
-        <div>
-            <h1>Glow Challenge Entries</h1>
-            <AccountTable>
-          <table>
-            <thead>
+  };
+  console.log(localAccounts);
+
+  return (
+    <div>
+      <h1>Glow Challenge Entries</h1>
+      <AccountTable>
+        <table>
+          <thead>
             <tr>
               <th id="filter">
                 <input
-                disabled={idInput}
-                id="id"
+                  disabled={idInput}
+                  id="glowEntryId"
                   type="text"
-                  onChange={(e) => {disableInput(e) ; handleChange(e)}}
-                />
-              </th>
-              <th id="filter">
-                <input 
-                disabled={nameInput}
-                id="name"
-                 type="text"
-                 onChange={(e) => {disableInput(e) ; handleChange(e)}}
-                  />
-              </th>
-              <th id="filter">
-                <input 
-                disabled={emailInput}
-                id="email"
-                type="text" 
-                onChange={(e) => {disableInput(e) ; handleChange(e)}}
+                  onChange={(e) => {
+                    disableInput(e);
+                    handleChange(e);
+                  }}
                 />
               </th>
               <th id="filter">
                 <input
-                disabled={ambassadorIdInput} 
-                id="ambassadorId"
-                type="text" 
-                onChange={(e) => {disableInput(e) ; handleChange(e)}}
-                 />
+                  disabled={nameInput}
+                  id="name"
+                  type="text"
+                  onChange={(e) => {
+                    disableInput(e);
+                    handleChange(e);
+                  }}
+                />
               </th>
               <th id="filter">
-              <select id="challenge" onChange={(e) => {disableInput(e) ; handleChange(e)}}>
-                <option value="">{' '}</option>
-                <option value="2021 Q2">2021 Q2</option>
-                <option value="2021 Q2">2021 Q3</option>
-                <option value="2021 Q2">2021 Q2</option>
-                <option value="2021 Q2">2021 Q2</option>
-              </select>
+                <input
+                  disabled={emailInput}
+                  id="email"
+                  type="text"
+                  onChange={(e) => {
+                    disableInput(e);
+                    handleChange(e);
+                  }}
+                />
               </th>
               <th id="filter">
-              <input type="checkbox" id='day' value='1' onChange={(e) => {handleChange(e)}} />
+                <input
+                  disabled={ambassadorIdInput}
+                  id="ambassadorId"
+                  type="text"
+                  onChange={(e) => {
+                    disableInput(e);
+                    handleChange(e);
+                  }}
+                />
               </th>
               <th id="filter">
-              <input type="checkbox" id='day' value='30' onChange={(e) => {handleChange(e)}} />
+                <select
+                  id="challenge"
+                  onChange={(e) => {
+                    disableInput(e);
+                    handleChange(e);
+                  }}
+                >
+                  <option value=""> </option>
+                  <option value="2021 Q2">2021 Q2</option>
+                  <option value="2021 Q2">2021 Q3</option>
+                  <option value="2021 Q2">2021 Q2</option>
+                  <option value="2021 Q2">2021 Q2</option>
+                </select>
               </th>
               <th id="filter">
-              <input type="checkbox" id='day' value='60' onChange={(e) => {handleChange(e)}} />
+                <input
+                  type="checkbox"
+                  id="day1Photo"
+                  value="1"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
               </th>
               <th id="filter">
-              <input type="checkbox" id='day' value='90' onChange={(e) => {handleChange(e)}} />
+                <input
+                  type="checkbox"
+                  id="day30Photo"
+                  value="30"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
               </th>
-            </tr><tr></tr>
-              <tr>
-                <th className="head">Entry ID
-                <CaretUp className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"id","asc")}}/>
-                <CaretDown className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"id","desc")}}/> 
-                </th>
-                <th className="head">Email
-                <CaretUp className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"email","asc")}}/>
-                <CaretDown className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"email","desc")}}/> 
-                </th>
-                <th className="head">Ambassador ID
-                <CaretUp className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"ambassadorId","asc")}}/>
-                <CaretDown className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"ambassadorId","desc")}}/> 
-                </th>
-                <th className="head">Name
-                <CaretUp className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"name","asc")}}/>
-                <CaretDown className="caretIcons" onClick={() => {accountsSort(perPage,pageNo,"name","desc")}}/> 
-                </th>
-                <th className="head">Challenge
-                </th>
-                <th className="head">1</th>
-                <th className="head">30</th>
-                <th className="head">60</th>
-                <th className="head">90</th>
-                <th className="head">Actions </th>
-              </tr>
-            </thead>
-            {localAccounts && localAccounts.data && localAccounts.data.length >= 1 && <tbody>
-              {localAccounts.data.map((user, i) => {
+              <th id="filter">
+                <input
+                  type="checkbox"
+                  id="day60Photo"
+                  value="60"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
+              </th>
+              <th id="filter">
+                <input
+                  type="checkbox"
+                  id="day90Photo"
+                  value="90"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
+              </th>
+            </tr>
+            <tr></tr>
+            <tr>
+              <th className="head">
+                Entry ID
+                <CaretUp
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "glowEntryId", "asc");
+                  }}
+                />
+                <CaretDown
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "glowEntryId", "desc");
+                  }}
+                />
+              </th>
+              <th className="head">
+                Email
+                <CaretUp
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "email", "asc");
+                  }}
+                />
+                <CaretDown
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "email", "desc");
+                  }}
+                />
+              </th>
+              <th className="head">
+                Ambassador ID
+                <CaretUp
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "ambassadorId", "asc");
+                  }}
+                />
+                <CaretDown
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "ambassadorId", "desc");
+                  }}
+                />
+              </th>
+              <th className="head">
+                Name
+                <CaretUp
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "name", "asc");
+                  }}
+                />
+                <CaretDown
+                  className="caretIcons"
+                  onClick={() => {
+                    accountsSort(perPage, pageNo, "name", "desc");
+                  }}
+                />
+              </th>
+              <th className="head">Challenge</th>
+              <th className="head">1</th>
+              <th className="head">30</th>
+              <th className="head">60</th>
+              <th className="head">90</th>
+              <th className="head">Actions </th>
+            </tr>
+          </thead>
+          <tbody>
+            {localAccounts.length > 1 &&
+              localAccounts.map((user, i) => {
                 return (
-                  <tr key={i} user = {user} id="row">
+                  <tr key={i} user={user} id="row">
                     <td>{user.glowEntryId}</td>
-                    <td>{user.email || 'test@testdata.com'}{blank}</td>
-                    <td>{user.ambassadorId || '7105698'}</td>
-                    <td>{user.name || 'test name'}</td>
-                    <td>{user.challenge || '2021 Q2'}</td>
-                    <td></td>
+                    <td>{user.email}</td>
+                    <td>{user.ambassadorId}</td>
+                    <td>{user.name}</td>
+                    <td>{user.challenge || "2021 Q2"}</td>
+                    <td>
+                      {user.day1Photo && (
+                        <img src={user.day1Photo} style={{ height: "35px" }} />
+                      )}
+                    </td>
                     <td></td>
                     <td></td>
                     <td></td>
@@ -209,17 +295,20 @@ function GlowEntryList() {
                       </Link>
                     </td>
                   </tr>
-                )
-              })}              
-              </tbody>
-            }
-          </table>
-          <h3>{message}</h3>
-        </AccountTable>
+                );
+              })}
+          </tbody>
+        </table>
+        <h3>{message}</h3>
+      </AccountTable>
 
-        <Pagination getEntries={getGlowEntries()} updatePerPage={updatePerPage} updatePageNo={updatePageNo} />
-        </div>
-    )
+      <Pagination
+        getEntries={getGlowEntries()}
+        updatePerPage={updatePerPage}
+        updatePageNo={updatePageNo}
+      />
+    </div>
+  );
 }
 
 export default GlowEntryList;
@@ -230,12 +319,12 @@ const AccountTable = styled.div`
 
   table {
     width: 100%;
-    
-     tr {
+
+    tr {
       &:nth-child(even) {
-        background: #F4FAFE;
+        background: #f4fafe;
       }
-      
+
       td {
         text-align: left;
         padding: 5px 3px;
@@ -256,6 +345,4 @@ const AccountTable = styled.div`
   #filter {
     text-align: left;
   }
-
-
 `;
